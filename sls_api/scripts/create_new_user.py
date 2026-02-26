4a04f7711b867b8fee7751e628bc4d1eaf7fa082
Rasmus Ek <rasmus.ek@sls.fi>
2026-02-26 16:22:14 +0200

    Helper script for creating new user accounts

---
+++ sls_api/scripts/create_new_user.py
@@ -0,0 +1,25 @@
+import argparse
+import sys
+
+from sls_api.models import User
+from sls_api import app
+
+if __name__ == "__main__":
+    with app.app_context():
+        parser = argparse.ArgumentParser(description="Helper script to create a new API user")
+        parser.add_argument("email", help="User email address")
+        parser.add_argument("password", help="User password")
+
+        args = parser.parse_args()
+
+        success = User.create_new_user(args.email, args.password)
+
+        if success is None:
+            print("Unexpected error creating user. Check API backend logs.")
+            sys.exit(1)
+        elif success:
+            print(f"User {args.email} created successfully!")
+            sys.exit(0)
+        else:
+            print(f"Unexpected error creating user. Check API backend logs.")
+            sys.exit(1)
