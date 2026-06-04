d77a6c9df211db09723397ac6dfbf0b90e676ceb
Rasmus Ek <rasmus.ek@sls.fi>
2026-06-04 13:47:20 +0300

    Use user.get_projects() to get projects as list

--- sls_api/endpoints/tools/publishing.py
+++ sls_api/endpoints/tools/publishing.py
@@ -73,7 +73,9 @@ def list_user_projects():
     # Get user in order to fetch projects from auth database
     identity = get_jwt_identity()
     user = User.find_by_id(int(identity["sub"]))
-    user_projects = list(user.projects)
+    user_projects = user.get_projects()
+    if not user_projects:    # handle cases where user projects is unset (None) or set incorrectly (empty string)
+        user_projects = []
     project_table = get_table("project")
 
     try:
